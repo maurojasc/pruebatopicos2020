fd99c46ac5dedb89b042139bc0df505628e4d5fb
Mauricio <Mauricio@DESKTOP-N1Q25V5>
2020-05-04 10:53:10 -0500

    Modificacion de MAuricio Rojas Primera modificación al proyecto con impresión inicial.

---
+++ topicos20201/src/topicos20201/Topicos20201.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topicos20201;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Topicos20201 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("modificación 1 mayo 4 hora 10:40 inicio del proyecto");
+    }
+    
+}
